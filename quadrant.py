43dfcf5a227bfc91947fb71b246cce97cdb7497f
olaruandreea <28634684+olaruandreea@users.noreply.github.com>
2019-08-14 21:33:54 +0100

    Add files via upload

--- quadrant.py
+++ quadrant.py
@@ -1,10 +1,10 @@
-X = int(input())
-Y = int(input())
-if X >= 0 and Y >= 0:
-    print "1"
-elif X > 0  and Y < 0:
-    print "4"
-elif X < 0 and Y < 0:
-    print "3"
-elif X < 0  and Y > 0:
-    print "2"
+x = int(input())
+y = int(input())
+if x >= 0 and y >= 0:
+    print 1
+elif x < 0 and y >= 0:
+    print 2
+elif x <= 0 and y <= 0:
+    print 3
+elif x>= 0 and y<=0:
+    print 4
