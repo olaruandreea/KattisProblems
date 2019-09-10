d236e95f3404d087c8ecabffcd9be4d8dfaf6b46
Andreea <28634684+olaruandreea@users.noreply.github.com>
2019-09-10 13:26:00 +0100

    Add files via upload

--- simon.py
+++ simon.py
@@ -1,15 +1,10 @@
 n = input()
-i = 1 
-l = []
-while i <= n:
+i = 0
+while i < int(n):
     s = raw_input()
-    tokens = s.split()
-    if tokens[0] == "Simon" and tokens[1] == "says":
-        l.append(tokens[2:])
+    if s[0:11] == "simon says ":
+      print s[11:]
+    else:
+      print ""
     i+=1
 
-for i in l:
-    new = ""
-    for t in i:
-        new += t + " "
-    print " " + new
