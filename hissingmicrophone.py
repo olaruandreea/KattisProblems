3a777d4791a7bab6e7448f6e8b9aa52ad19f9ac9
Andreea <28634684+olaruandreea@users.noreply.github.com>
2019-11-13 20:28:58 +0000

    Add files via upload

--- hissingmicrophone.py
+++ hissingmicrophone.py
@@ -1,5 +1,9 @@
-s = raw_input()
-if "ss" in s:
-    print "hiss"
-else:
-    print "no hiss"
+def check_if_hissing(user_input):
+	if "ss" in user_input:
+		return "hiss"
+	else:
+		return "no hiss"
+if __name__ == '__main__':
+	user_input = raw_input()
+	print check_if_hissing(user_input)
+
